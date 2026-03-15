fe83cd821f477f213c41d61306474d2b46f2809f
SPARTAN <gigi.onyegbula.2007@gmail.com>
2026-03-15 23:59:56 +0100

    BIRTH

---
+++ src/pieces/Piece.ts
@@ -0,0 +1,22 @@
+export type Color = "white" | "black";
+
+export type Position = {
+	row: number;
+	col: number;
+};
+
+export class Piece {
+	type: string;
+	color: Color;
+	position: Position;
+
+	constructor(type: string, color: Color, position: Position) {
+		this.type = type;
+		this.color = color;
+		this.position = position;
+	}
+
+	moveTo(position: Position) {
+		this.position = position;
+	}
+}
